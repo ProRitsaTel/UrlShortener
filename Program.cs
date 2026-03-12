478879aaaba3f7b77facec1d888fbcfb8feebbbc
Влад <vkondrahov2014@gmail.com>
2026-03-13 00:56:57 +0300

    fix

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using UrlShortener.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
-                      ?? throw new InvalidOperationException("Connection string not found.");
+    ?? throw new InvalidOperationException("Connection string not found.");
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
@@ -13,29 +13,14 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
-}
-
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
+using var scope = app.Services.CreateScope();
+scope.ServiceProvider.GetRequiredService<AppDbContext>().Database.Migrate();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
-app.MapControllerRoute(
-    name: "shortUrl",
-    pattern: "{code}",
-    defaults: new { controller = "Home", action = "Go" });
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute("shortUrl", "{code}", new { controller = "Home", action = "Go" });
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
